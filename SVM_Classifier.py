75b29fe5708808bc213c889f6ba929758eaa997a
alex-mont <44395427+alex-mont@users.noreply.github.com>
2019-12-26 18:46:54 +0100

    up

--- SVM_Classifier.py
+++ SVM_Classifier.py
@@ -5,15 +5,56 @@ import shutil
 from PIL import Image
 import mahotas as mt
 import sklearn.svm as skl
+from sklearn.metrics import accuracy_score
 
 #####################################__UTILS__#######################################
 # data augmentation
-def mirroring(path):
-    mass_images = os.listdir(path)
+def mirroring(mass_path, overlay_images, ground_images):
+    n_mass = 1
+    n_overlay = 1
+    n_ground = 1
+    mass_images = os.listdir(mass_path)
     for image in mass_images:
-        img = Image.open(path + '\\' + image)
-        rotated_image = img.transpose(Image.FLIP_LEFT_RIGHT)
-        rotated_image.save(path + '\\' + 'rotated' + image)
+        print("Mirroring mass image n.", n_mass)
+        img = Image.open(mass_path + '\\' + image)
+        mirror_image = img.transpose(Image.FLIP_LEFT_RIGHT)
+        mirror_image.save(mass_path + '\\' + 'mirror' + image)
+        n_mass += 1
+    for image in overlay_images:
+        print("Mirroring overlay image n.", n_overlay)
+        img = Image.open('dataset\overlay' + '\\' + image)
+        mirror_image = img.transpose(Image.FLIP_LEFT_RIGHT)
+        mirror_image.save('dataset\overlay' + '\\' + 'mirror' + image)
+        n_overlay += 1
+    for image in ground_images:
+        print("Mirroring ground image n.", n_ground)
+        img = Image.open('dataset\groundtruth' + '\\' + image)
+        mirror_image = img.transpose(Image.FLIP_LEFT_RIGHT)
+        mirror_image.save('dataset\groundtruth' + '\\' + 'mirror' + image)
+        n_ground += 1
+
+def flipping(mass_images, nomass_images, ground_images):
+    n_mass = 1
+    n_nomass = 1
+    n_ground = 1
+    for image in mass_images:
+        print("Flipping mass image n.", n_mass)
+        img = Image.open("dataset\images\mass\\" + image)
+        rotated = img.rotate(45)
+        rotated.save("dataset\images\mass\ROT_" + image)
+        n_mass += 1
+    for image in nomass_images:
+        print("Flipping nomass image n.", n_nomass)
+        img = Image.open("dataset\images\\nomass\\" + image)
+        rotated = img.rotate(45)
+        rotated.save("dataset\images\\nomass\ROT_" + image)
+        n_nomass += 1
+    for image in ground_images:
+        print("Flipping ground image n.", n_ground)
+        img = Image.open("dataset\groundtruth\\" + image)
+        rotated = img.rotate(45)
+        rotated.save("dataset\groundtruth\ROT_" + image)
+        n_ground += 1
 
 # create labelled classes
 def createClasses(source_path, all_images, overlay_images, dest_path):
@@ -23,10 +64,30 @@ def createClasses(source_path, all_images, overlay_images, dest_path):
         if image in overlay_images:
             mass_images.append(image)
             shutil.move(source_path + '\\' + image, dest_path) #it also remove the file from the source directory
-            print("Processing image number: " + str(num_mass))
+            print("Processing image n. " + str(num_mass))
             num_mass += 1
 
-def clearing(mask_images, all_images):
+def createTestSet(mass_path, nomass_path, test_path):
+    mass_images = os.listdir(mass_path)
+    #Takes 10% mass images for the test set
+    for i in range(45):
+        print("Moving image n.", i)
+        shutil.move(mass_path + '\\' + mass_images[i], test_path)
+
+    nomass_images = os.listdir(nomass_path)
+    #Takes 10% mass images for the test set
+    for i in range(60):
+        print("Moving image n.", i)
+        shutil.move(nomass_path + '\\' + nomass_images[i], test_path)
+    '''
+    RENAME FILES
+    test_images = os.listdir(test_path)
+    for image in test_images:
+        os.rename(test_path + "\\" + image, test_path + "\\" + "NOMASS_" + image)
+    '''
+
+# removing background noise
+def cleaning(mask_images, all_images):
     count = 1
     for image in all_images:
         img_name = image[:-4]
@@ -35,19 +96,9 @@ def clearing(mask_images, all_images):
                 img = cv.imread("dataset\images\\nomass" + '\\' + image)
                 cutter = cv.imread("dataset\masks" + '\\' + mask)
                 img[cutter == 0] = 0 #apply the mask on the image in order to clean the background
-                print("Cleaning image number " + str(count))
+                print("Cleaning image n. " + str(count))
                 count += 1
                 break
-
-def flipping(mass_images, nomass_images):
-    for image in mass_images:
-        img = Image.open("dataset\images\mass\\" + image)
-        rotated = img.rotate(45)
-        rotated.save("dataset\images\mass\ROT_" + image)
-    for image in nomass_images:
-        img = Image.open("dataset\images\\nomass\\" + image)
-        rotated = img.rotate(45)
-        rotated.save("dataset\images\\nomass\ROT_" + image)
 
 def extract_features(image):
     # calculate haralick texture features for 4 types of adjacency
@@ -72,6 +123,23 @@ def store(train_features,train_labels):
         np.savetxt(features_file,train_features)
     with open("Labels.txt", "w") as labels_file:
         np.savetxt(labels_file,train_labels)
+
+def evaluate_prediction(y_pred, y_true):
+    predicted_mass = 0
+    predicted_nomass = 0
+    for y in y_pred:
+        if y == 0:
+            predicted_nomass +=1
+        else:
+            predicted_mass +=1
+    print("Number of masses: 45")
+    print("Number of predicted masses: ", predicted_mass)
+    print("---------------------------------------")
+    print("Number of non-masses: 60")
+    print("Number of predicted non-masses: ", predicted_nomass)
+    print("---------------------------------------")
+    print("SVM ACCURACY: ", accuracy_score(y_true, y_pred))
+
 ################################__END UTILS__##########################################
 
 # load the training dataset
@@ -84,70 +152,76 @@ mass_images = os.listdir(mass_path)
 overlay_path = "dataset\overlay"
 overlay_images = os.listdir(overlay_path)
 
+# declare test set
+test_path = "dataset\\test"
+
 # load the mask dataset
 mask_path = "dataset\masks"
 mask_images = os.listdir(mask_path)
 
-# load the test set
-test_path = "dataset\\test"
-test_images = os.listdir(test_path)
+#load the groundtruth
+ground_path = "dataset\groundtruth"
+ground_images = os.listdir(ground_path)
 
 labelling = False #set True if you want to create class labels
 if(labelling):
-    clearing(mask_images, nomass_images)
+    cleaning(mask_images, nomass_images)
     createClasses(nomass_path, nomass_images, overlay_images, mass_path)
-    mirroring(mass_path)
-    flipping(mass_images, nomass_images)
+    mirroring(mass_path, overlay_images, ground_images)
+    flipping(mass_images, nomass_images, ground_images)
+    createTestSet(mass_path, nomass_path, test_path)
+
 
 # empty list to hold feature vectors and train labels
 train_features = []
 train_labels = []
 
-# Extracting features from the training samples by using Haralick - GLCM
+# Extracting features from the training samples by using Haralick
 print ("--------- [STATUS]: Started extracting haralick textures... ---------")
 count_training = 1
 if not check_file():
     for mass in mass_images:
         # read the training image
         image = cv.imread("dataset\images\mass\\" + mass, cv.IMREAD_GRAYSCALE)
-        train_labels.append(0)
+        train_labels.append(int(1))
         # extract haralick texture from the image
         features = extract_features(image)
         # append the feature vector and label
         train_features.append(features)
-
         print("Extracting features from image number " + str(count_training))
         count_training += 1
+
     for nomass in nomass_images:
         image = cv.imread("dataset\images\\nomass\\" + nomass, cv.IMREAD_GRAYSCALE)
-        train_labels.append(1)
+        train_labels.append(int(0))
         # extract haralick texture from the image
         features = extract_features(image)
         # append the feature vector and label
         train_features.append(features)
-
         print("Extracting features from image number " + str(count_training))
         count_training += 1
-    #store(train_features,train_labels)
+
     store(train_features,train_labels)
 else:
     train_features,train_labels = load()
 
 # Create the classifier
 print ("--------- [STATUS] Creating the classifier... ---------")
-clf_svm = skl.SVC(C=1.5, kernel='rbf', gamma='auto', verbose=True, max_iter=-1)
+svm = skl.SVC(C=100000, kernel='rbf', gamma='auto', verbose=True, max_iter=-1)
 
 # Fit the training data and labels
 print ("--------- [STATUS]: Fitting data/label to model... ---------")
-clf_svm.fit(train_features, train_labels)
+svm.fit(train_features, train_labels)
 
-test_path = "dataset\\test"
+# load the test set
 test_images = os.listdir(test_path)
 
-count_test = 1
 # Loop over the test images
+count_test = 1
+y_pred = []
+y_true = []
 for test in test_images:
-    image = cv.imread("dataset\\test\\" + test)
+    image = cv.imread(test_path + "\\" + test)
     # convert to grayscale
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
@@ -155,12 +229,20 @@ for test in test_images:
     features = extract_features(gray)
 
     # evaluate the model and predict label
-    prediction = clf_svm.predict(features.reshape(1, -1))[0]
+    prediction = int(svm.predict(features.reshape(1, -1))[0])
     print("------------------------------")
-    # 1 means noMass, while 0 means mass
     print("Prediction of image number ", str(count_test), ": ", prediction)
 
-    # show the label
+    y_pred.append(prediction)
+
+    # show the label : 0 means noMass, while 1 means mass
     count_test += 1
 
+# Evaluate SVM prediction
+for i in range(105):
+    if i < 45:
+        y_true.append(int(1))
+    else:
+        y_true.append(int(0))
 
+evaluate_prediction(y_pred, y_true)
